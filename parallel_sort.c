f166a1d24d9efc215a9cdab4b82d707c8b69877a
igorgiamattey <igorlopesgiamattey@gmail.com>
2026-03-31 12:12:13 -0300

    Added original sort algorithm, without parallelism and renamed the parallelised one

--- parallel_sort.c
+++ parallel_sort.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 
+#define TRESHOLD_SIZE 100000
 #define CLOCK_MONOTONIC 1
 
 void merge(int *arr, int left, int mid, int right) {
@@ -37,13 +38,22 @@ void merge(int *arr, int left, int mid, int right) {
 
 void merge_sort(int *arr, int left, int right) {
     if (left < right) {
-        int mid = (left + right) / 2;
-        merge_sort(arr, left, mid);
-        merge_sort(arr, mid + 1, right);
+        int mid = (left + right)/2;
+
+        if (right - left < TRESHOLD_SIZE){
+            merge_sort(arr, left, mid);
+            merge_sort(arr, mid + 1, right);
+        }
+        else {
+            #pragma omp task
+            merge_sort(arr, left, mid);
+            #pragma omp task
+            merge_sort(arr, mid + 1, right);
+        }
+        #pragma omp taskwait
         merge(arr, left, mid, right);
     }
 }
-
 
 int partition(int *arr, int low, int high) {
     int pivot = arr[high]; // pivot simples
@@ -68,11 +78,12 @@ int partition(int *arr, int low, int high) {
 void quick_sort(int *arr, int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
+        #pragma omp task
         quick_sort(arr, low, pi - 1);
+        #pragma omp task
         quick_sort(arr, pi + 1, high);
     }
 }
-
 
 double get_time() {
     struct timespec t;
@@ -85,13 +96,20 @@ void fill_random(int *arr, int n) {
         arr[i] = rand();
 }
 
+int check_sort(int *arr, int n){
+    for (int i = 0; i < n-1; ++i){
+        if (arr[i] > arr[i+1])
+            return 0;
+    }
+    return 1;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Uso: %s <tamanho>\n", argv[0]);
         return 1;
     }
-
+    
     int n = atoi(argv[1]);
     srand(42);
 
@@ -101,23 +119,52 @@ int main(int argc, char *argv[]) {
 
     fill_random(base, n);
 
-    memcpy(arr1, base, n * sizeof(int));
     memcpy(arr2, base, n * sizeof(int));
+    memcpy(arr1, base, n * sizeof(int));
 
     // -------- Quick Sort --------
-    double t3 = get_time();
-    quick_sort(arr2, 0, n - 1);
-    double t4 = get_time();
-
-    // -------- Merge Sort --------
+    
     double t1 = get_time();
-    merge_sort(arr1, 0, n - 1);
+
+    #pragma omp parallel
+    {
+         #pragma omp single
+        {
+            merge_sort(arr1, 0, n - 1);
+        }
+    }
+
     double t2 = get_time();
+    printf("Merge Sort: %.6f s\n", t2 - t1);
+    
+    double t3 = get_time();
+    
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            quick_sort(arr2, 0, n - 1);
+        }
+    }
+
+    double t4 = get_time();
+    printf("Quick Sort: %.6f s\n", t4 - t3);
+    
+    printf("QS: ");
+    if (check_sort(arr2, n))
+        puts("=-=-=-=-=-= IT'S SORTED =-=-=-=-=-=");
+    else
+        puts("=-=-=-=-=-= IT AIN'T SORTED =-=-=-=-=-=");
+
+    printf("MS: ");
+    if (check_sort(arr1, n))
+        puts("=-=-=-=-=-= IT'S SORTED =-=-=-=-=-=");
+    else
+        puts("=-=-=-=-=-= IT AIN'T SORTED =-=-=-=-=-=");
 
     
     printf("Tamanho: %d\n", n);
-    printf("Merge Sort: %.6f s\n", t2 - t1);
-    printf("Quick Sort: %.6f s\n", t4 - t3);
+    
     
     
     /*
